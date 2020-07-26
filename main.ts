ae9fc2ba0e2299840526de614d2fdbf5bee7eee3
sahawut <sahawut@gmail.com>
2020-07-26 11:43:11 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,16 +2,18 @@ input.onButtonPressed(Button.A, function () {
     strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Violet))
     strip.show()
 })
-input.onButtonPressed(Button.B, function () {
-    strip.rotate(1)
-    strip.show()
+input.onButtonPressed(Button.AB, function () {
+    reset()
 })
+function reset () {
+    strip.showRainbow(1, 360)
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    strip.show()
+}
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P1, 4, NeoPixelMode.RGB)
 strip.setBrightness(20)
-strip.showRainbow(1, 360)
-strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
-strip.show()
+reset()
 basic.forever(function () {
 	
 })
